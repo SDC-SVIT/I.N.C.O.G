20df2bb962f82ae6d252f24030127e4379d12528
Ayaan Retiwala <gr88mind@gmail.com>
2020-02-02 18:54:02 +0530

    location got

--- I.N.C.O.G App/app/src/main/java/sih/hexclan/incog/MainActivity.java
+++ I.N.C.O.G App/app/src/main/java/sih/hexclan/incog/MainActivity.java
@@ -1,13 +1,28 @@
 package sih.hexclan.incog;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private static final long LOCATION_RATE_GPS_MS = TimeUnit.SECONDS.toMillis(1L);
+    private static final long LOCATION_RATE_NETWORK_MS = TimeUnit.SECONDS.toMillis(60L);
+    private LocationManager mLocationManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +31,63 @@ public class MainActivity extends AppCompatActivity {
         Fragment mapFragment = new MapFragment();
         FragmentManager fragmentManager = this.getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout,mapFragment);
+        fragmentTransaction.replace(R.id.frame_layout, mapFragment);
         fragmentTransaction.commit();
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        registerLocation();
+    }
+
+
+    private LocationListener mLocationListener = new LocationListener() {
+
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.d(TAG, "onLocationChanged: " + location.getLatitude() + " " + location.getLongitude());
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    public void registerLocation() {
+        boolean isGpsProviderEnabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (isGpsProviderEnabled) {
+            if (checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    Activity#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for Activity#requestPermissions for more details.
+                return;
+            }
+            Log.d(TAG, "registerLocation: in here");
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    LOCATION_RATE_NETWORK_MS,
+                    0.0f /* minDistance */,
+                    mLocationListener);
+
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    LOCATION_RATE_GPS_MS,
+                    0.0f /* minDistance */,
+                    mLocationListener);
+        }
     }
 }
