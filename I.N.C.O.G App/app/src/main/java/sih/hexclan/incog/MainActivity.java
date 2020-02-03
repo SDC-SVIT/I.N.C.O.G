087885784f3265e726e203ccc5666798deafdec4
DaScientist <37672507+DaScientist@users.noreply.github.com>
2020-02-03 08:25:02 +0530

    mastan

--- I.N.C.O.G App/app/src/main/java/sih/hexclan/incog/MainActivity.java
+++ I.N.C.O.G App/app/src/main/java/sih/hexclan/incog/MainActivity.java
@@ -6,11 +6,15 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
 
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.ImageFormat;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -28,45 +32,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private LocationManager mLocationManager;
     private static final int LOCATION_PERMISSION_REQUEST = 1;
-    private static final String[] REQUIRED_PERMISSIONS = { Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.CAMERA };
-    private LocationListener mLocationListener = new LocationListener() {
-
-        @Override
-        public void onLocationChanged(Location location) {
-            Log.d(TAG, "onLocationChanged: " + location.getLatitude() + " " + location.getLongitude());
-            Toast.makeText(MainActivity.this,
-                    "onLocationChanged: " + location.getLatitude() + " " + location.getLongitude(), Toast.LENGTH_SHORT)
-                    .show();
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-
-        }
-    };
-
-    // private OnNmeaMessageListener nmeaMessageListener = new
-    // OnNmeaMessageListener() {
-    // @Override
-    // public void onNmeaMessage(String message, long timestamp) {
-    //// Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
-    // String[] split = message.split(",");
-    // Log.d(TAG, "onNmeaMessage: " + message);
-    //
-    // }
-    // };
-
+    private static final String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.CAMERA};
     static LatLng requiredLocation;
 
     @Override
@@ -84,11 +51,59 @@ public class MainActivity extends AppCompatActivity {
         // setNmeaMessageListener();
 
     }
+//    public void activateClick(View view) {
+////        Fragment cameraFrag = new CameraFragment();
+//        Toast.makeText(this, "Click Activated!! Latlong are:"+MainActivity.requiredLocation.latitude+" "+MainActivity.requiredLocation.longitude, Toast.LENGTH_SHORT).show();
+//        Log.d(TAG, "activateClick: Latlong are : "+MainActivity.requiredLocation.latitude+" "+MainActivity.requiredLocation.longitude);
+//        try {
+//            Navigation.findNavController(this,R.id.frame_layout).navigate(MapFragmentDirections.actionMapFragmentToCameraFragment(((CameraManager) getActivity().getSystemService(Context.CAMERA_SERVICE)).getCameraIdList()[0], ImageFormat.JPEG));
+//
+//        } catch (CameraAccessException e) {
+//            e.printStackTrace();
+//        }
+////        this.getSupportFragmentManager().beginTransaction().replace(R.id.cameraFragment, cameraFrag).commit();
+//    }
+}
 
-    public void activateClick(View view) {
-        Fragment cameraFrag = new CameraFragment();
-        this.getSupportFragmentManager().beginTransaction().replace(R.id.cameraFragment, cameraFrag).commit();
-    }
+
+
+    //    private LocationListener mLocationListener = new LocationListener() {
+//
+//        @Override
+//        public void onLocationChanged(Location location) {
+//            Log.d(TAG, "onLocationChanged: " + location.getLatitude() + " " + location.getLongitude());
+//            Toast.makeText(MainActivity.this,
+//                    "onLocationChanged: " + location.getLatitude() + " " + location.getLongitude(), Toast.LENGTH_SHORT)
+//                    .show();
+//        }
+//
+//        @Override
+//        public void onStatusChanged(String provider, int status, Bundle extras) {
+//
+//        }
+//
+//        @Override
+//        public void onProviderEnabled(String provider) {
+//
+//        }
+//
+//        @Override
+//        public void onProviderDisabled(String provider) {
+//
+//        }
+//    };
+
+    // private OnNmeaMessageListener nmeaMessageListener = new
+    // OnNmeaMessageListener() {
+    // @Override
+    // public void onNmeaMessage(String message, long timestamp) {
+    //// Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+    // String[] split = message.split(",");
+    // Log.d(TAG, "onNmeaMessage: " + message);
+    //
+    // }
+    // };
+
     //
     // public void setNmeaMessageListener() {
     // Toast.makeText(this, "setNmeaMessageListener: Listener added",
@@ -123,4 +138,4 @@ public class MainActivity extends AppCompatActivity {
     // super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     // setNmeaMessageListener();
     // }
-}
+
--- I.N.C.O.G App/app/src/main/java/sih/hexclan/incog/MapFragment.java
+++ I.N.C.O.G App/app/src/main/java/sih/hexclan/incog/MapFragment.java
@@ -2,21 +2,17 @@ package sih.hexclan.incog;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.ImageFormat;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.OnNmeaMessageListener;
-import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,12 +20,16 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdate;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
@@ -41,10 +41,10 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     private LocationManager mLocationManager;
     private static final int LOCATION_PERMISSION_REQUEST = 1;
-    private static final String[] REQUIRED_PERMISSIONS = { Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.CAMERA };
+    private static final String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.CAMERA};
     SharedPreferences sharedPreferences;
-
+    Marker marker;
     private LocationListener mLocationListener = new LocationListener() {
 
         @Override
@@ -89,11 +89,28 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         mapView.onCreate(savedInstanceState);
         sharedPreferences = getActivity().getApplicationContext().getSharedPreferences("perm", Context.MODE_PRIVATE);
 
+        root.findViewById(R.id.startSnapping).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                nt cameraFrag = new CameraFragment();
+                    Toast.makeText(getActivity(), "Click Activated!! Latlong are:"+MainActivity.requiredLocation.latitude+" "+MainActivity.requiredLocation.longitude, Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "activateClick: Latlong are : "+MainActivity.requiredLocation.latitude+" "+MainActivity.requiredLocation.longitude);
+                    try {
+                        Navigation.findNavController(getActivity(),R.id.frame_layout).navigate(MapFragmentDirections.actionMapFragmentToCameraFragment(((CameraManager) getActivity().getSystemService(Context.CAMERA_SERVICE)).getCameraIdList()[0], ImageFormat.JPEG));
+
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+//        this.getSupportFragmentManager().beginTransaction().replace(R.id.cameraFragment, cameraFrag).commit();
+                }
+
+        });
+
         mLocationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
         if (getActivity()
                 .checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && getActivity().checkSelfPermission(
-                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.putBoolean("perm", true);
             requestPermissions(REQUIRED_PERMISSIONS, LOCATION_PERMISSION_REQUEST);
@@ -108,7 +125,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         if (getActivity()
                 .checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && getActivity().checkSelfPermission(
-                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
             return;
 
         if (isGpsProviderEnabled) {
@@ -132,17 +149,33 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         // .position(svit)
         // .title("INCOG")
         // );
+
         this.googleMap.moveCamera(CameraUpdateFactory.zoomTo(18));
         this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(svit));
         this.googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
-                googleMap.addMarker(new MarkerOptions().position(latLng).title("Required Location"));
+                if (marker!=null){marker.remove();}
+                marker = googleMap.addMarker(new MarkerOptions().position(latLng).title("Required Location"));
                 MainActivity.requiredLocation = latLng;
                 ((Button) root.findViewById(R.id.startSnapping)).setVisibility(View.VISIBLE);
             }
         });
     }
+
+    public void activateClick(View view) {
+//        Fragment cameraFrag = new CameraFragment();
+        Toast.makeText(getActivity(), "Click Activated!! Latlong are:"+MainActivity.requiredLocation.latitude+" "+MainActivity.requiredLocation.longitude, Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "activateClick: Latlong are : "+MainActivity.requiredLocation.latitude+" "+MainActivity.requiredLocation.longitude);
+        try {
+            Navigation.findNavController(getActivity(),R.id.frame_layout).navigate(MapFragmentDirections.actionMapFragmentToCameraFragment(((CameraManager) getActivity().getSystemService(Context.CAMERA_SERVICE)).getCameraIdList()[0], ImageFormat.JPEG));
+
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+//        this.getSupportFragmentManager().beginTransaction().replace(R.id.cameraFragment, cameraFrag).commit();
+    }
+
 
     @Override
     public void onDestroy() {
